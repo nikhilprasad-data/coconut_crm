04c11ab5e07577ceb9b66f0fe950cc5f28dc1f37
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-04-04 16:28:57 +0530

    feat(api): implement complete CRUD for purchase and payment with strict RBAC

--- src/routes/payment.py
+++ src/routes/payment.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify,request
 from flask_jwt_extended import get_jwt_identity,jwt_required
 from src.models.payment import Payment
 from src.models.seller import Seller
 from src.models.user import User
+from src.db import db
 
 payment_bp = Blueprint('payment', __name__, url_prefix='/api/payment')
 
@@ -65,3 +66,287 @@ def get_payment(seller_id):
                          "status"  : "error",
                          "message" : "Internal server error"
           }), 500
+     
+@payment_bp.route('/add', methods= ['POST'])
+@jwt_required()
+def add_payment():
+     try:
+          current_identity    = int(get_jwt_identity())
+          current_user        = User.query.get(current_identity)
+
+          if not current_user:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "User not found"
+               }), 404
+          
+          if current_user.role == 'Seller':
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Access Denied. As seller you can't add payment" 
+               }), 403
+          
+          data = request.get_json()
+
+          required_fields = ["seller_id", "payment_date", "amount_paid", "payment_method"]
+
+          for field in required_fields:
+
+               if field not in data or data[field] is None:
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : f"{field} is missing or empty. Please provide all the details"
+                    }), 400
+               
+          try:
+               seller_id = int(data["seller_id"])
+
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : f"Please input required seller_id in correct data type format"
+               }), 400
+          
+          existing_seller = Seller.query.get(seller_id)
+
+          if not existing_seller:
+               return jsonify({
+                              "status" : "error",
+                              "message" : "Seller not found"
+               }), 404
+
+          try:
+               payment_date   = str(data["payment_date"])
+               amount_paid    = float(data["amount_paid"])
+               payment_method = str(data["payment_method"]).strip().title()
+
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Please input required fields in correct data type format"
+               }), 400
+          
+          new_payment = Payment(seller_id= seller_id, payment_date= payment_date, amount_paid= amount_paid, payment_method= payment_method)
+          db.session.add(new_payment)
+          db.session.commit()
+          return jsonify({
+                         "status"  : "success",
+                         "message" : f"Successfully added payment on seller id {seller_id}"
+               }), 200
+     
+     except Exception as e:
+          print(e)
+          return jsonify({
+                         "status"  : "error",
+                         "message" : "Internal server error"
+          }), 500
+
+@payment_bp.route('/update/<int:payment_id>', methods= ['PATCH'])
+@jwt_required()
+def update_payment(payment_id):
+     try:
+          current_identity    = int(get_jwt_identity())
+          current_user        = User.query.get(current_identity)
+
+          if not current_user:
+               return jsonify({
+                              "status"  : "success",
+                              "message" : "User not found"
+               }), 404
+          
+          if current_user.role == 'Seller':
+               return jsonify({
+                              "status"  :"error",
+                              "message" : "Access Denied. As seller you can't update payment" 
+               }), 403
+          
+          existing_payment = Payment.query.get(payment_id)
+
+          if not existing_payment:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Payment not found"
+               }), 404
+          
+          data = request.get_json()          
+          updated_feilds = []
+
+          if "seller_id" in data:
+               try:
+                    seller_id = int(data.get("seller_id"))
+                    existing_seller = Seller.query.get(seller_id)
+
+                    if not existing_seller:
+                         return jsonify({
+                                        "status" : "error",
+                                        "message": "Seller not found"
+                         }), 404
+                    
+                    existing_payment.seller_id = seller_id
+                    updated_feilds.append({"seller_id" : seller_id})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : "Wrong data type for seller_id"
+                    }), 400
+               
+          if "payment_date" in data:
+               try:
+                    payment_date = str(data.get("payment_date"))
+                    
+                    existing_payment.payment_date = payment_date
+                    updated_feilds.append({"payment_date" : payment_date})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : "Wrong data type for payment_date"
+                    }), 400
+          
+          if "amount_paid" in data:
+               try:
+                    amount_paid = float(data.get("amount_paid"))
+                    
+                    existing_payment.amount_paid = amount_paid
+                    updated_feilds.append({"amount_paid" : amount_paid})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : "Wrong data type for amount_paid"
+                    }), 400
+          
+          if "payment_method" in data:
+               try:
+                    payment_method = str(data.get("payment_method")).strip().title()
+                    
+                    existing_payment.payment_method = payment_method
+                    updated_feilds.append({"payment_method" : payment_method})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : "Wrong data type for payment_method"
+                    }), 400
+          if len(updated_feilds) == 0:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "No valid fields provided for update"
+               }), 400
+               
+          db.session.commit()
+          return jsonify({
+                         "status"            : "success",
+                         "message"           : f"Payment {payment_id}, updated successfully",
+                         "field_updated"     : updated_feilds
+          }), 200
+     
+     except Exception as e:
+          print(e)
+          return jsonify({
+                         "status"  : "error",
+                         "message" : "Internal server error"
+          }), 500 
+     
+@payment_bp.route('/replace/<int:payment_id>', methods= ['PUT'])
+@jwt_required()
+def replace_payment(payment_id):
+     try:
+          current_identity    = int(get_jwt_identity())
+          current_user        = User.query.get(current_identity)
+
+          if not current_user:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "User not found"
+               }), 404
+          
+          if current_user.role == 'Seller':
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Access Denied. Seller can't replace payment"
+               }), 403
+          
+          existing_payment = Payment.query.get(payment_id)
+
+          if not existing_payment:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Payment not found"
+               }), 404
+          
+          data = request.get_json()
+
+          required_fields = ["seller_id", "payment_date", "amount_paid", "payment_method"]
+
+          for field in required_fields:
+
+               if field not in data or data[field] is None:
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : f"{field} is missing or empty. Please provide all the details"
+                    }), 400
+               
+          replaced_data = []
+          try:
+               seller_id = int(data.get("seller_id"))
+               existing_seller = Seller.query.get(seller_id)
+               replaced_data.append({"seller_id" : seller_id})
+
+               if not existing_seller:
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : "Seller not found"
+                    }), 404
+               
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Wrong data type for seller_id"
+               }), 400
+          
+          try:
+               payment_date = str(data.get("payment_date"))
+               replaced_data.append({"payment_date" : payment_date})
+
+               amount_paid = float(data.get("amount_paid"))
+               replaced_data.append({"amount_paid" : amount_paid})
+
+               payment_method = str(data.get("payment_method")).strip().title()
+               replaced_data.append({"payment_method" : payment_method})
+          
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Please input required fields in correct data type format"
+               }), 400
+          
+          existing_payment.seller_id         = seller_id
+          existing_payment.payment_date      = payment_date
+          existing_payment.amount_paid       = amount_paid
+          existing_payment.payment_method    = payment_method
+
+          db.session.commit()
+
+          return jsonify({
+                         "status"            : "success",
+                         "message"           : f"Payment {payment_id}, replaced successfully",
+                         "replaced_data"     : replaced_data
+
+          }), 200
+     
+     except Exception as e:
+          print(e)
+          return jsonify({
+                         "status"            : "error",
+                         "message"           : "Internal server error"
+          }), 500 
--- src/routes/purchase.py
+++ src/routes/purchase.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required,get_jwt_identity
 from src.models.seller import Seller
 from src.models.purchase import Purchase
 from src.models.user import User
+from src.db import db
 
 purchase_bp = Blueprint('purchase', __name__, url_prefix='/api/purchase')
 
@@ -65,3 +66,306 @@ def get_purchase(seller_id):
                          "status"  : "error",
                          "message" : "Internal server error"
           }), 500
+
+@purchase_bp.route('/add', methods= ['POST'])
+@jwt_required()
+def add_purchase():
+     try:
+          current_identity    = int(get_jwt_identity())
+          current_user        = User.query.get(current_identity)
+
+          if not current_user:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "User not found"
+               }), 404
+          
+          if current_user.role == 'Seller':
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Access Denied. As a seller you can't add purchase."
+               }), 403
+          
+          data  = request.get_json()
+
+          required_fields = ["seller_id", "purchase_date", "total_bags", "waste_pieces", "rate_per_piece"]
+
+          for field in required_fields:
+
+               if field not in data or data[field] is None:
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : f"{field} is missing or empty. Please provide all the details"
+                    }), 400
+          try:
+               seller_id = int(data["seller_id"])
+
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : f"Please input required seller_id in correct data type format"
+               }), 400
+          
+          existing_seller = Seller.query.get(seller_id)
+
+          if not existing_seller:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Seller not found"
+               }), 404
+          
+          try:
+               purchase_date  = str((data)["purchase_date"])
+               total_bags     = int((data)["total_bags"])
+               waste_pieces   = int((data)["waste_pieces"])
+               rate_per_piece = float((data)["rate_per_piece"])
+          
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Please input required fields in correct data type format"
+               }), 400
+          
+          new_purchase = Purchase(seller_id= seller_id, purchase_date= purchase_date, total_bags= total_bags, waste_pieces= waste_pieces, rate_per_piece= rate_per_piece)
+          db.session.add(new_purchase)
+          db.session.commit()
+          return jsonify({
+                         "status"  : "success",
+                         "message" : f"Successfully added purchase on seller id {seller_id}"
+               }), 200
+     
+     except Exception as e:
+          print(e)
+          return jsonify({
+                         "status"  : "error",
+                         "message" : "Internal server error"
+          }), 500
+
+@purchase_bp.route('/update/<int:purchase_id>', methods= ['PATCH'])
+@jwt_required()
+def update_purchase(purchase_id):
+     try:
+          current_identity = int(get_jwt_identity())
+          current_user     = User.query.get(current_identity)
+
+          if not current_user:
+               return jsonify({
+                              "status"  :  "error",
+                              "message" : "User not found"
+               }), 404
+        
+          if current_user.role == 'Seller':
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Access Denied. Seller can't update any payment"
+               }), 403
+        
+          existing_purchase = Purchase.query.get(purchase_id)
+
+          if not existing_purchase:
+             return jsonify({
+                              "status"  : "error",
+                              "message" : "Payment not found"
+             }), 404
+        
+          data = request.get_json()
+          updated_fields = []
+
+          if "seller_id" in data:
+               try:
+                    seller_id = int(data.get("seller_id")) 
+                    existing_seller = Seller.query.get(seller_id)
+
+                    if not existing_seller:
+                         return jsonify({
+                                        "status"  : "error",
+                                        "message" : "Seller not found"
+                         }), 404
+                         
+                    existing_purchase.seller_id   = seller_id
+                    updated_fields.append({"seller_id": seller_id})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                              "status"  : "error",
+                              "message" : "Wrong data type for seller_id"
+                    }), 400
+          
+          if "purchase_date" in data:
+               try:
+                    purchase_date                      = str(data.get("purchase_date"))
+                    existing_purchase.purchase_date    = purchase_date
+
+                    updated_fields.append({"purchase_date" : purchase_date})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                              "status"  : "error",
+                              "message" : "Wrong data type for purchase_date"
+                    }), 400
+          
+          if "total_bags" in data:
+               try:
+                    total_bags                    = int(data.get("total_bags"))
+                    existing_purchase.total_bags  = total_bags
+
+                    updated_fields.append({"total_bags" : total_bags})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                              "status"  : "error",
+                              "message" : "Wrong data type for total_bags"
+                    }), 400
+               
+          if "waste_pieces" in data:
+               try:
+                    waste_pieces                       = int(data.get("waste_pieces"))
+                    existing_purchase.waste_pieces     = waste_pieces
+
+                    updated_fields.append({"waste_pieces" : waste_pieces})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                              "status"  : "error",
+                              "message" : "Wrong data type for waste_pieces"
+                    }), 400
+          
+          if "rate_per_piece" in data:
+               try:
+                    rate_per_piece                     = float(data.get("rate_per_piece"))
+                    existing_purchase.rate_per_piece   = rate_per_piece
+
+                    updated_fields.append({"rate_per_piece" : rate_per_piece})
+
+               except Exception as e:
+                    print(e)
+                    return jsonify({
+                              "status"  : "error",
+                              "message" : "Wrong data type for rate_per_piece"
+                    }), 400
+          
+          if len(updated_fields) == 0:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "No valid fields provided for update"
+               }), 400
+          
+          db.session.commit()
+          return jsonify({
+                         "status"            : "success",
+                         "message"           : f"Purchase {purchase_id} updated successfully",
+                         "fields_updated"    : updated_fields
+          }), 200
+          
+     except Exception as e:
+          print(e)
+          return jsonify({
+                         "status"  : "error",
+                         "message" : "Internal server error"
+          }), 500
+
+@purchase_bp.route('/replace/<int:purchase_id>', methods= ['PUT'])
+@jwt_required()
+def replace_purchase(purchase_id):
+     try:
+          current_identity    = int(get_jwt_identity())
+          current_user        = User.query.get(current_identity)
+
+          if not current_user:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "User not found"
+               }), 404
+          
+          if current_user.role == 'Seller':
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Access Denied. Seller can't replace purchase"
+               }), 403
+          
+          existing_purchase = Purchase.query.get(purchase_id)
+
+          if not existing_purchase:
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Purchase not found"
+               }), 404
+          
+          data = request.get_json()
+
+          required_fields = ["seller_id", "purchase_date", "total_bags", "waste_pieces", "rate_per_piece"]
+
+          for field in required_fields:
+
+               if field not in data or data[field] is None:
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : f"{field} is missing or empty. Please provide all the details"
+                    }), 400
+               
+          replaced_data = []
+          try:
+               seller_id = int(data.get("seller_id"))
+               existing_seller = Seller.query.get(seller_id)
+               replaced_data.append({"seller_id" : seller_id})
+
+               if not existing_seller:
+                    return jsonify({
+                                   "status"  : "error",
+                                   "message" : "Seller not found"
+                    }), 404
+               
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Wrong data type for seller_id"
+               }), 400
+          
+          try:
+               purchase_date = str(data.get("purchase_date"))
+               replaced_data.append({"purchase_date" : purchase_date})
+
+               total_bags = float(data.get("total_bags"))
+               replaced_data.append({"total_bags" : total_bags})
+
+               waste_pieces = str(data.get("waste_pieces"))
+               replaced_data.append({"waste_pieces" : waste_pieces})
+
+               rate_per_piece = str(data.get("rate_per_piece"))
+               replaced_data.append({"rate_per_piece" : rate_per_piece})
+
+          except Exception as e:
+               print(e)
+               return jsonify({
+                              "status"  : "error",
+                              "message" : "Please input required fields in correct data type format"
+               }), 400
+          
+          existing_purchase.seller_id        = seller_id
+          existing_purchase.purchase_date    = purchase_date
+          existing_purchase.total_bags       = total_bags
+          existing_purchase.waste_pieces     = waste_pieces
+          existing_purchase.rate_per_piece   = rate_per_piece
+
+          db.session.commit()
+
+          return jsonify({
+                         "status"            : "success",
+                         "message"           : f"Purchase {purchase_id}, replaced successfully",
+                         "replaced_data"     : replaced_data
+
+          }), 200
+     
+     except Exception as e:
+          print(e)
+          return jsonify({
+                         "status"            : "error",
+                         "message"           : "Internal server error"
+          }), 500 
