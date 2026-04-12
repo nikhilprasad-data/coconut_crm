9ea43b63f45e1e14c912a24d56aa7d15828623bd
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-04-12 06:13:09 +0530

    chore(seed): add demo admin credentials for seamless interviewer login

--- seed_admin.py
+++ seed_admin.py
@@ -20,15 +20,29 @@ with app.app_context():
           password_hash  =    generate_password_hash(password)
           role           =    'admin'
 
-          existing_user  = User.query.filter_by(role= 'admin').first()
+          demo_username       =    "demo_admin_11"
+          demo_password       =    "demo_password"
+          demo_password_hash  =    generate_password_hash(demo_password)
+          demo_role           =    'admin'
+
+          existing_user       =    User.query.filter_by(username= username).first()
+          demo_existing_user  =    User.query.filter_by(username= demo_username).first()
 
           if existing_user:
                print("Admin already exists")
           else:
-               admin = User(username= username, password_hash= password_hash, role= role)
+               admin     = User(username= username, password_hash= password_hash,  role= role)
                db.session.add(admin)
                db.session.commit()
                print("Admin created successfully")
 
+          if demo_existing_user:
+               print("Demo Admin already exists")
+          else:
+               demo_admin     = User(username= demo_username, password_hash= demo_password_hash,  role= demo_role)
+               db.session.add(demo_admin)
+               db.session.commit()
+               print("Demo Admin created successfully")
+               
      except Exception as e:
           print(e)
